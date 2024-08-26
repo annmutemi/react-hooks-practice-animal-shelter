d4128e06050c990cce3f599c850ee6f4c6009592
annmutemi <annshiru92@gmail.com>
2024-08-26 21:41:10 +0300

    Animal shelter

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,33 @@
 import React, { useState } from "react";
-
+ 
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
-
+import data from "../db.json"
+ 
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
-
+ 
+  const onChangeType = (type) => {
+    setFilters({ ...filters, type });
+  };
+ 
+  const onFindPetsClick = () => {
+    let filteredPets;
+    if (filters.type === "all") {
+      filteredPets = data.pets;
+    } else {
+      filteredPets = data.pets.filter((pet) => pet.type === filters.type);
+    }
+    setPets(filteredPets);
+  };
+ 
+  const onAdoptPet = (id) => {
+    setPets(pets.map((pet) =>
+      pet.id === id ? { ...pet, isAdopted: true } : pet
+    ));
+  };
+ 
   return (
     <div className="ui container">
       <header>
@@ -15,15 +36,15 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={onChangeType} onFindPetsClick={onFindPetsClick}  />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={onAdoptPet} />
           </div>
         </div>
       </div>
     </div>
   );
 }
-
+ 
 export default App;
